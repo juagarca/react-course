7e5c163cd9c451fafd0504aeb28ff3bd1b0c6bb6
Juan Garcia <juagarca@gmail.com>
2022-03-11 15:16:35 +0000

    Add Battle component

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
+import Battle from './components/Battle'
 import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
