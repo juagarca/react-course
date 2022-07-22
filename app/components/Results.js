d162868e88c158a86037d3098d7b1dfc1c1b9dd1
Juan Garcia <juagarca@gmail.com>
2022-07-22 15:30:03 +0100

    Add player score logic and api call

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,21 @@
-import * as React from 'react'
+import React from 'react'
+import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        console.log('data: ', players)
+      })
+  }
+
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
-      </div>
+        < pre > {JSON.stringify(this.props, null, 2)}</pre >
+      </div >
     )
   }
 }
